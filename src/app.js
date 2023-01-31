55f4ac0261bc657044bec08e8829abe5f06be060
paul michael <mopaul@student.lautech.edu.ng>
2023-01-31 15:54:21 +0100

    set up everything and about to dive into postman

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
-console.log('hello world!');
+// console.log('hello world!');
 
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-console.log(uuidv4());
+// console.log(uuidv4());
+
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+app.get('/', (req, res) => {
+    res.send(`hello world!`);
+})
+
+app.post('/', (req,  res) => {
+    res.send('This is a POST request');
+})
+
+app.listen(port, () => {console.log(`Example of app listening port ${port}`)});
